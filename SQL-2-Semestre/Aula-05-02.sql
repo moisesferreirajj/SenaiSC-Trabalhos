5bbebee43b8a12a948121a97f7d16e1849a885c6
Moises João Ferreira <154389705+moisesferreirajj@users.noreply.github.com>
2025-02-05 14:24:15 -0300

    Add files via upload

--- SQL-2-Semestre/Aula-05-02.sql
+++ SQL-2-Semestre/Aula-05-02.sql
@@ -1,8 +1,8 @@
-#####
-# DBZINHA BASICA DO MOISES J. FERREIRA
-# 05/02/2025
-# TDESN V3
-#####
+#############################################
+# DBZINHA BASICA DO MOISES J. FERREIRA		#
+# 05/02/2025								#
+# TDESN V3									#
+#############################################
 
 create database Escola;
 use Escola;
@@ -15,14 +15,16 @@ create table aluno(
   primary key (PK_Matricula)
 );
 
-create table turmas_has_aluno(
-  Pk_Turmas_has_Aluno int not null auto_increment,
-  Fk_turma int,
-  Fk_matricula int,
-  foreign key (Fk_turma) references turmas(Pk_turma),
-  foreign key (Fk_matricula) references aluno(PK_Matricula)
-  on delete restrict on update cascade
-);
+INSERT INTO aluno (Nome_Aluno, Endereco_Aluno, Cpf_Aluno) 
+VALUES 
+('Carlos Silva', 'Rua A, 123', 12345678901),
+('Fernanda Oliveira', 'Av. B, 456', 23456789012),
+('Lucas Pereira', 'Rua C, 789', 34567890123),
+('Mariana Costa', 'Av. D, 1011', 45678901234),
+('João Santos', 'Rua E, 1213', 56789012345),
+('Rafaela Almeida', 'Rua F, 1415', 67890123456),
+('Gabriel Souza', 'Av. G, 1617', 78901234567),
+('Aline Rocha', 'Rua H, 1819', 89012345678);
 
 create table professor(
   Pk_professor int not null auto_increment,
@@ -31,12 +33,34 @@ create table professor(
   primary key (Pk_professor)
 );
 
+INSERT INTO professor (Nome_professor, Titulacao_professor)
+VALUES 
+('João Ferreira', 'Graduação'),
+('Maria Souza', 'Mestre'),
+('Carlos Rocha', 'Doutorado'),
+('Renata Lima', 'Mestre'),
+('Fernanda Costa', 'Mestre'),
+('Luis Silva', 'Doutorado'),
+('Patricia Oliveira', 'Doutorado'),
+('Antonio Pereira', 'Graduação');
+
 create table turmas(
   Pk_turma int not null auto_increment,
   Sigla_turma varchar(8),
   Data_inicio date,
   primary key (Pk_turma)
 );
+
+INSERT INTO turmas (Sigla_turma, Data_inicio)
+VALUES 
+('TDESI', '2025-02-10'),
+('TMEC', '2025-02-12'),
+('TELET', '2025-03-05'),
+('TQUIM', '2025-03-10'),
+('TDESI2', '2025-02-10'),
+('TMEC2', '2025-02-12'),
+('TELET2', '2025-03-05'),
+('TQUIM2', '2025-03-10');
 
 create table disciplina(
   Pk_disciplina int not null auto_increment,
@@ -48,3 +72,35 @@ create table disciplina(
   foreign key (Fk_professor) references professor(Pk_professor)
   on delete restrict on update cascade
 );
+
+INSERT INTO disciplina (Nome_disciplina, Fk_turma, Fk_professor)
+VALUES 
+('Português', 1, 1),
+('Matemática', 2, 2),
+('Banco de Dados', 3, 3),
+('Análise', 4, 4),
+('Lógica', 5, 5),
+('Front-end', 6, 6),
+('Back-end', 7, 7),
+('Elétrica', 8, 8);
+
+create table turmas_has_aluno(
+  Pk_Turmas_has_Aluno int not null auto_increment,
+  Fk_turma int,
+  Fk_matricula int,
+  primary key (Pk_Turmas_has_Aluno),
+  foreign key (Fk_turma) references turmas(Pk_turma),
+  foreign key (Fk_matricula) references aluno(PK_Matricula)
+  on delete restrict on update cascade
+);
+
+INSERT INTO turmas_has_aluno (Fk_turma, Fk_matricula)
+VALUES 
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6),
+(7, 7),
+(8, 8);
